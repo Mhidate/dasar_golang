f75500eccab9d74472d85f429fbdaf03399c5b51
mhidate <ayot427@gmail.com>
2025-03-25 22:29:43 +0800

    variabel

--- basic/variabel/main.go
+++ basic/variabel/main.go
@@ -2,6 +2,9 @@ package main
 
 import "fmt"
 
+// Variabel global
+var globalVar = "Saya bisa diakses di mana saja"
+
 func main() {
 	// Deklarasi dengan tipe data
 	var name string = "jhon"
@@ -28,4 +31,16 @@ func main() {
 
 	fmt.Println(firstName3, lastName3, age3)
 
+	// Variabel lokal
+	var localVar = "Saya hanya bisa diakses di dalam main()"
+
+	fmt.Println(globalVar)
+	fmt.Println(localVar)
+
+	var a int     // Default: 0
+	var b string  // Default: ""
+	var c bool    // Default: false
+	var d float64 // Default: 0.0
+
+	fmt.Println(a, b, c, d)
 }
