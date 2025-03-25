fee786fab2ac7656fb697c510095353b86a7d426
mhidate <ayot427@gmail.com>
2025-03-25 17:10:25 +0800

    variabel

--- basic/variabel/main.go
+++ basic/variabel/main.go
@@ -12,4 +12,20 @@ func main() {
 
 	fmt.Println(name, age, city)
 
+	//short declaration
+	name2 := "Doe"
+	age2 := 10
+	city2 := "Medan"
+
+	fmt.Println(name2, age2, city2)
+
+	//deklarasi banyak variabel sekaligus
+	var (
+		firstName3 = "John"
+		lastName3  = "Doe"
+		age3       = 30
+	)
+
+	fmt.Println(firstName3, lastName3, age3)
+
 }
