78babeb577f0df277afe504872ecb982a28a6d67
mhidate <ayot427@gmail.com>
2025-03-31 17:14:35 +0800

    type declarations lanjut

--- basic/Type-declarations/main.go
+++ basic/Type-declarations/main.go
@@ -3,6 +3,8 @@ package main
 import "fmt"
 
 func main() {
+
+	//Type Declaration di Golang adalah fitur yang memungkinkan kita untuk membuat alias atau mendefinisikan tipe data baru berdasarkan tipe data yang sudah ada.
 	fmt.Println("Belajar type declarations")
 
 	type name string
@@ -19,9 +21,9 @@ func main() {
 	fmt.Println(umur)
 	fmt.Println("----------------")
 
-	fmt.Println("Mencoba penjumlahan dengan tipe asli dan type declarations")
+	fmt.Println("Mencoba penjumlahan dengan tipe asli dan alias")
 
-	//nama variabel tidak boleh sama dengan nama type declarations
+	//nama variabel tidak boleh sama dengan nama type declarations atau alias
 	type angka1 int16
 	type angka2 int16
 
@@ -31,7 +33,63 @@ func main() {
 
 	fmt.Println(angka12)
 	fmt.Println(angka22)
+	fmt.Println("---------------")
 	// fmt.Println(angka12 + angka22) tidak bisa melakukan operasi dengan type declarations yang berbeda atau type data asli
-	// fmt.Println(angka12 + angka3)
+	// fmt.Println(angka12 + angka3) variabel angka3 harus di ubah ke type angka1 agar bisa menjalankan operasi
 
+	fmt.Println("Mencoba membuat type declarations dengan struct")
+
+	type Animal struct {
+		Nama    string
+		Habitat string
+	}
+
+	var hewan Animal = Animal{Nama: "Ikan lele", Habitat: "Air Tawar"}
+	fmt.Println(hewan.Nama, hewan.Habitat)
+	fmt.Println("---------------")
+
+	fmt.Println("Mencoba membuat type declarations dengan Interface")
+
+	var s Speaker = Human{Name: "Alisia"}
+	fmt.Println(s.Speak())
+	fmt.Println("---------------")
+	fmt.Println("---------------")
+
+	fmt.Println("Mencoba membuat type declarations dengan Function")
+	var operation AddFunc = add
+	fmt.Println(operation(3, 4))
+	fmt.Println("---------------")
+
+	fmt.Println("Mencoba membuat type declarations dengan Slice dan Map")
+	type Names []string         // Alias untuk slice string
+	type Role map[string]string // Alias untuk map[string]string
+
+	var pegawai Names = []string{"Alice", "Bob", "Charlie"}
+	fmt.Println(pegawai)
+
+	var staff Role = map[string]string{
+		"backend":  "Backend Go",
+		"frontend": "Frontend Vue.js",
+	}
+	fmt.Println(staff["backend"])
+
+}
+
+type Speaker interface {
+	Speak() string
+}
+
+type Human struct {
+	Name string
+}
+
+func (h Human) Speak() string {
+
+	return "Hello, my name is " + h.Name
+}
+
+type AddFunc func(int, int) int // Alias untuk function yang menerima dua int dan mengembalikan int
+
+func add(a int, b int) int {
+	return a + b
 }
