7850bb022804c842c2264f0d508e51e194ebdc44
mhidate <ayot427@gmail.com>
2025-04-01 21:58:45 +0800

    operasi matematika awal

---
+++ basic/Operasi-matematika/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Belajar operasi matematika")
+
+	a := 10
+	b := 3
+	c := 3.0
+	d := 2.0
+
+	fmt.Println("Penjumlahan:", a+b)
+	fmt.Println("Pengurangan:", a-b)
+	fmt.Println("Perkalian:", a*b)
+	fmt.Println("Pembagian:", c/d)
+	fmt.Println("Modulus:", a%b)
+}
