a6c293d46268d9cb94ad31ccabfa0f5d9bd89a9b
mhidate <ayot427@gmail.com>
2025-04-03 18:59:04 +0800

    operator perbandingan

---
+++ basic/operasi-perbandingan/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Belajar operasi perbandingan")
+
+	a := 10
+	b := 20
+
+	fmt.Println("Apakah a == b?", a == b)
+	fmt.Println("Apakah a != b?", a != b)
+	fmt.Println("Apakah a > b?", a > b)
+	fmt.Println("Apakah a < b?", a < b)
+
+}
