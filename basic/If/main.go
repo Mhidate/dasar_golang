4844c036551efc03729125b313e1fe788a609d29
mhidate <ayot427@gmail.com>
2025-04-08 19:26:40 +0800

    If

---
+++ basic/If/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Belajar percabangan atau IF")
+
+	// if mengeksekusi kondisi tertentu
+
+	nilai := 66
+
+	if nilai >= 70 {
+		fmt.Println("C")
+	} else if nilai >= 80 {
+		fmt.Println("B")
+	} else if nilai > 90 {
+		fmt.Println("A")
+	} else {
+		fmt.Println("D")
+	}
+
+	//IF dengan short statement
+	if skor := 70; skor >= 60 {
+		fmt.Println("Lulus")
+	}
+
+	if panjang := len("golang"); panjang > 5 {
+		fmt.Println("Panjang string lebih dari 5:", panjang)
+	} else {
+		fmt.Println("Pendek")
+	}
+
+}
