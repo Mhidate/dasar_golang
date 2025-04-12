04179c752e6ddde903f22d2861d10b2967e4556e
mhidate <ayot427@gmail.com>
2025-04-12 22:09:48 +0800

    Function lanjut

--- basic/Function/main.go
+++ basic/Function/main.go
@@ -7,7 +7,38 @@ import "fmt"
 func salam() {
 	fmt.Println("Halo,selamat siang")
 }
+func eksekusi(f func(string)) {
+	f("Dewi")
+}
+
+func sapa(nama string) {
+	fmt.Println("Hai", nama)
+}
+
+func pembuatSapa() func(string) {
+	return func(nama string) {
+		fmt.Println("Hai", nama)
+	}
+}
+
 func main() {
 	fmt.Println("Belajar tentang Function")
+
 	salam()
+
+	//Function bisa diperlakukan seperti data lainnya, misalnya:
+	//Disimpan dalam variabel
+	greet := func(nama string) {
+		fmt.Println("Halo", nama)
+	}
+
+	greet("Andi")
+
+	//Dikirim sebagai parameter ke fungsi lain
+	eksekusi(sapa)
+
+	//Dikembalikan dari fungsi
+	sapa := pembuatSapa()
+	sapa("Budi")
+
 }
