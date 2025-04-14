dfbf921e16bd1e53ab83d26e074a5aa9b25af41d
mhidate <ayot427@gmail.com>
2025-04-14 19:42:25 +0800

    function parameter

--- basic/Function-parameter/main.go
+++ basic/Function-parameter/main.go
@@ -29,6 +29,50 @@ func kurang(a, b int) int {
 	return a - b
 }
 
+//struct sebagai parameter
+type User struct {
+	Nama string
+	Id   int
+}
+
+func cetakUser(u User) {
+	fmt.Println("Nama:", u.Nama)
+	fmt.Println("ID:", u.Id)
+}
+
+//slice sebagai parameter
+func cetakSlice(data []string) {
+	for i, v := range data {
+		fmt.Printf("Index %d: %s\n", i, v)
+	}
+}
+
+//array sebagai parameter
+func cetakArray(data [3]int) {
+	for i, v := range data {
+		fmt.Printf("Index %d: %d\n", i, v)
+	}
+}
+
+//map sebagai parameter
+func cetakMap(m map[string]string) {
+	for k, v := range m {
+		fmt.Printf("%s: %s\n", k, v)
+	}
+}
+
+//interface sebagai parameter
+func cetakInterface(data interface{}) {
+	fmt.Println("Isi interface:", data)
+}
+
+//slice dan interface sebagai parameter
+func cetakSemua(data []interface{}) {
+	for i, v := range data {
+		fmt.Printf("Index %d: %v\n", i, v)
+	}
+}
+
 func main() {
 	fmt.Println("Belajar function parameter")
 
@@ -43,5 +87,30 @@ func main() {
 	d := 6
 	b := kurang(c, d)
 	fmt.Println(c, "-", d, "=", b)
+
+	user := User{
+		Nama: "Andi",
+		Id:   123,
+	}
+	cetakUser(user)
+
+	nama := []string{"Andi", "Budi", "Citra"}
+	cetakSlice(nama)
+
+	angka1 := [3]int{10, 20, 30}
+	cetakArray(angka1)
+
+	data := map[string]string{
+		"nama":  "Dewi",
+		"email": "dewi@example.com",
+	}
+	cetakMap(data)
+
+	cetakInterface("Halo Dunia")
+	cetakInterface(123)
+	cetakInterface([]int{1, 2, 3})
+
+	koleksi := []interface{}{"Teks", 42, true}
+	cetakSemua(koleksi)
 
 }
