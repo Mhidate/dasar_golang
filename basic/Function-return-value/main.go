bb99793584d4f1a8127cbc38e0d00d13d4d7c0e2
mhidate <ayot427@gmail.com>
2025-04-16 23:17:04 +0800

    Function return value

---
+++ basic/Function-return-value/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func tambah(a int, b int) int {
+	return a + b
+}
+
+func main() {
+	fmt.Println("Belajar function return value")
+
+	//Fungsi di Go bisa mengembalikan nilai setelah dijalankan. Nilai ini disebut return value.
+	//Fungsi dengan return harus menggunakan keyword return.
+
+	hasil := tambah(3, 4)
+	fmt.Println("Hasil:", hasil)
+
+}
