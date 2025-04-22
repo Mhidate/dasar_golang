0fb9b5a6d25de93b10b709813fcf1d14239f13b9
mhidate <ayot427@gmail.com>
2025-04-22 19:58:52 +0800

    Function return multiple values lanjut

--- basic/Function-return-multiple-values/main.go
+++ basic/Function-return-multiple-values/main.go
@@ -9,13 +9,38 @@ func kali(a, b int) (int, bool) {
 	return a * b, true
 }
 
+func biodata(nama, alamat, status string) (string, string, string) {
+	return nama, alamat, status
+}
+
+func cariData(index int) (string, error) {
+	data := []string{"A", "B", "C"}
+	if index < 0 || index >= len(data) {
+		return "", fmt.Errorf("Index out of range")
+	}
+	return data[index], nil
+}
+
 func main() {
 	fmt.Println("Belajar fungsi yang mengembalikan banyak nilai")
+	//Go memungkinkan sebuah fungsi untuk mengembalikan lebih dari satu nilai
 
 	hasil, sukses := kali(10, 8)
 	if sukses {
 		fmt.Println("Hasil:", hasil)
 	} else {
 		fmt.Println("Pembagian tidak valid")
+	}
+
+	nama, alamat, status := biodata("Jhone", "London", "Lajang")
+	fmt.Println("Nama lengkap:", nama)
+	fmt.Println("Alamat:", alamat)
+	fmt.Println("Status:", status)
+
+	nilai, err := cariData(1)
+	if err != nil {
+		fmt.Println("Terjadi error:", err)
+	} else {
+		fmt.Println("Data ditemukan:", nilai)
 	}
 }
