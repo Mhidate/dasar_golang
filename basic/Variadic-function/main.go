7b78f902707b12a82882db16a50c0f2d5b3c18a6
mhidate <ayot427@gmail.com>
2025-04-25 22:15:31 +0800

    Variadic function lanjut

--- basic/Variadic-function/main.go
+++ basic/Variadic-function/main.go
@@ -16,12 +16,26 @@ func cetak(pesan ...string) {
 	}
 }
 
+func biodata(umur int, bio ...string) {
+
+	fmt.Println("Nama   :", bio[0])
+	fmt.Println("Alamat :", bio[1])
+	fmt.Println("Status :", bio[2])
+	fmt.Println("Umur : ", umur)
+
+}
+
 func main() {
 	fmt.Println("Belajar tetang variadic function")
 
-	//Variadic function adalah fungsi yang bisa menerima parameter dengan sejumlah tak terbatas
+	//Variadic function adalah fungsi yang bisa menerima parameter dengan sejumlah tak terbatas.
+	//Parameter variadic harus berada di akhir.
+	//Parameter variadic tidak boleh lebih dari satu.
+
 	fmt.Println(tambah(4, 5, 3, 8))
 
 	cetak("halo,jangan lupa cari kerja", ",jangan mencuri", "waulaupun lagi susah")
+
+	biodata(30, "John", "London", "Lajang")
 
 }
