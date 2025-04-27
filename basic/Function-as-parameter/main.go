b73523e98bdcb24e48890d011e5f00adfbdd83f5
mhidate <ayot427@gmail.com>
2025-04-27 13:20:35 +0800

    Fuction as parameter

--- basic/Function-as-parameter/main.go
+++ basic/Function-as-parameter/main.go
@@ -6,6 +6,10 @@ func cetakHasil(angka int, proses func(int) int) {
 	fmt.Println("hasil ", angka, "x", "9 =", proses(angka))
 }
 
+func operasi(a, b int, proses func(int, int) int) int {
+	return proses(a, b)
+}
+
 func main() {
 	fmt.Println("Belajar function as parameter")
 	//Fungsi yang menerima parameter berupa fungsi lain.
@@ -15,5 +19,18 @@ func main() {
 	}
 
 	cetakHasil(6, kaliSembilan)
+
+	tambah := func(x, y int) int {
+		return x + y
+	}
+
+	hasil := operasi(5, 3, tambah)
+	fmt.Println("Hasil tambah:", hasil)
+
+	// atau pakai anonymous function langsung
+	hasilKali := operasi(5, 3, func(x, y int) int {
+		return x * y
+	})
+	fmt.Println("Hasil kali:", hasilKali)
 
 }
