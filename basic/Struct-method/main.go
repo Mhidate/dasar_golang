628c264bfa5e5b7e8cb09947cbee0570018372da
mhidate <ayot427@gmail.com>
2025-05-05 18:59:48 +0800

    Struct method

--- basic/Struct-method/main.go
+++ basic/Struct-method/main.go
@@ -7,6 +7,13 @@ type Produk struct {
 	Harga int
 }
 
+// Struct Biodata
+type Biodata struct {
+	Nama   string
+	Alamat string
+	Umur   int
+}
+
 // Method value receiver
 func (p Produk) Info() {
 	fmt.Println("Produk:", p.Nama, "Harga:", p.Harga)
@@ -15,6 +22,14 @@ func (p Produk) Info() {
 // Method pointer receiver
 func (p *Produk) UbahHarga(hargaBaru int) {
 	p.Harga = hargaBaru
+}
+
+// Method untuk menampilkan data biodata
+func (b Biodata) Tampilkan() {
+	fmt.Println("Nama  :", b.Nama)
+	fmt.Println("Alamat:", b.Alamat)
+	fmt.Println("Umur  :", b.Umur, "tahun")
+	fmt.Println("-----------------------------")
 }
 
 func main() {
@@ -29,4 +44,14 @@ func main() {
 
 	p.UbahHarga(9500000)
 	p.Info()
+
+	daftarBiodata := []Biodata{
+		{Nama: "Budi", Alamat: "Jakarta", Umur: 15},
+		{Nama: "John", Alamat: "London", Umur: 35},
+		{Nama: "Karen", Alamat: "Texas", Umur: 29},
+	}
+
+	for _, biodata := range daftarBiodata {
+		biodata.Tampilkan()
+	}
 }
