085a5f596e09cd1fdb9c6daba9779e1ddf9d9869
mhidate <ayot427@gmail.com>
2025-05-07 20:26:20 +0800

    Tipe data slice

--- basic/Tipe-data-slice/main.go
+++ basic/Tipe-data-slice/main.go
@@ -9,14 +9,26 @@ type Materi struct {
 
 func main() {
 	fmt.Println("Belajar tipe data slice")
+	/*
+	  Slice di Go adalah potongan dari array yang dinamis panjangnya dan tidak memerlukan alokasi ulang.
+	*/
 
 	buah := []string{"apel", "mangga", "jeruk"}
+
+	//Cek panjang
+	fmt.Println(len(buah))
+
+	//Cek kapasitas
+	fmt.Println(cap(buah))
 
 	// Tambah elemen
 	buah = append(buah, "pisang")
 
-	// Akses elemen
-	fmt.Println(buah[2]) // jeruk
+	// Akses elemen dengan index
+	fmt.Println(buah[2])
+
+	//Mengubah nilai elemen slice
+	buah[0] = "jambu"
 
 	// Looping
 	for i, val := range buah {
@@ -24,8 +36,17 @@ func main() {
 	}
 
 	// Slicing
-	bagian := buah[1:3] // ["mangga", "jeruk"]
+	bagian := buah[1:3]
 	fmt.Println("Bagian:", bagian)
+
+	cariBuah := "Anggur"
+	for _, value := range buah {
+		if value == cariBuah {
+			fmt.Println("Buah yang anda cari ada di slice")
+			break
+		}
+		fmt.Println("Buah yang anda cari tidak ada di slice")
+	}
 
 	//slice of struct
 	materi := []Materi{
