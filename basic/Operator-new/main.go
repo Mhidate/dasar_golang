b91c2b93867e197a97bc252bac9db10cf7b94b0e
mhidate <ayot427@gmail.com>
2025-05-18 21:36:47 +0800

    Operator new

--- basic/Operator-new/main.go
+++ basic/Operator-new/main.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
+type User struct {
+	Nama string
+	Umur int
+}
+
 func main() {
 	fmt.Println("Belajar Operator new di pointer")
 
@@ -19,4 +24,26 @@ func main() {
 	*p = 100
 
 	fmt.Println("Nilai setelah diubah:", *p)
+
+	user1 := User{"Dewi", 22}
+	ptrUser1 := &user1
+
+	fmt.Println("\nNama user1:", ptrUser1.Nama)
+	fmt.Println("Umur user1:", ptrUser1.Umur)
+
+	// Ubah lewat pointer
+	ptrUser1.Umur = 23
+	fmt.Println("Umur user1 setelah diubah:", user1.Umur)
+
+	ptrUser2 := new(User) // User{} zero value → Nama "" Umur 0
+
+	fmt.Println("\nNilai awal Nama user2:", ptrUser2.Nama)
+	fmt.Println("Nilai awal Umur user2:", ptrUser2.Umur)
+
+	// Isi data lewat pointer
+	ptrUser2.Nama = "Rina"
+	ptrUser2.Umur = 30
+
+	fmt.Println("Nama user2 setelah diisi:", ptrUser2.Nama)
+	fmt.Println("Umur user2 setelah diisi:", ptrUser2.Umur)
 }
