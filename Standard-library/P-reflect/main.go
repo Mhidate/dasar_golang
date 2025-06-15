6227eb3411bff79bef02c89916be7a23ab548a2a
mhidate <ayot427@gmail.com>
2025-06-15 16:48:07 +0800

    Paket reflect

--- Standard-library/P-reflect/main.go
+++ Standard-library/P-reflect/main.go
@@ -5,6 +5,12 @@ import (
 	"reflect"
 )
 
+type User struct {
+	Nama  string
+	Umur  int
+	Email string
+}
+
 func main() {
 
 	fmt.Println("Belajar paket reflect")
@@ -19,6 +25,8 @@ func main() {
 			generic log & debugging tool
 			Karena Go itu statically typed language, tapi kadang di runtime kita pengen bisa tahu tipe, nama, atau isi field dari suatu data tanpa hardcoded — nah di situ reflect dipakai.
 	*/
+
+	// Dapatkan Tipe & Value
 	var x int = 42
 
 	t := reflect.TypeOf(x)
@@ -26,4 +34,22 @@ func main() {
 
 	fmt.Println("Tipe :", t)
 	fmt.Println("Value:", v)
+
+	// Iterasi Field Struct Pakai Reflect
+	u := User{"Dian", 25, "dian@mail.com"}
+
+	val := reflect.ValueOf(u)
+	typ := reflect.TypeOf(u)
+
+	for i := 0; i < val.NumField(); i++ {
+		fmt.Printf("Field %d: %s = %v\n", i, typ.Field(i).Name, val.Field(i))
+	}
+
+	// Set Value Dinamis (harus pointer)
+	var y int = 10
+	z := reflect.ValueOf(&y).Elem()
+
+	fmt.Println("Sebelum:", y)
+	z.SetInt(99)
+	fmt.Println("Sesudah:", y)
 }
