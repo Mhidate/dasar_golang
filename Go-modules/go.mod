bd848459f9640054fbfc539b10c098fd12567d0e
mhidate <ayot427@gmail.com>
2025-06-24 23:15:44 +0800

    go modules

---
+++ Go-modules/go.mod
@@ -0,0 +1,7 @@
+module go-modules
+
+go 1.24.1
+
+require github.com/gosimple/slug v1.15.0
+
+require github.com/gosimple/unidecode v1.0.1 // indirect
---
+++ Go-modules/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gosimple/slug"
+)
+
+func main() {
+	text := "Belajar Go Modules!"
+	/*
+		Go Modules adalah sistem dependency management bawaan Go yang diperkenalkan di Go 1.11 (stabil di Go 1.13).
+		Modul adalah kumpulan kode Go dalam sebuah direktori yang berisi file go.mod.
+		Fungsinya:
+		Menyimpan informasi tentang dependencies (package lain yang dibutuhkan)
+		Mengatur versi dependency yang dipakai
+		Memudahkan pembuatan, pengelolaan, dan distribusi project Go
+	*/
+
+	/*
+		Perintah Dasar Go Modules
+		go mod init	,Inisialisasi modul
+		go mod tidy,	Download missing deps & hapus unused
+		go mod download,	Download semua dependency
+		go mod verify,	Verifikasi checksum di go.sum
+		go list -m all,	Lihat semua modul dan versinya
+		go get	,Update/tambah dependency
+	*/
+
+	fmt.Println(slug.Make(text))
+}
